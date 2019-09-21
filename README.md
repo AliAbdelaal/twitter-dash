6e251db86f24b9eddb3c946fe368b223c7a56331
Ali Abdel Aal <aliabdelaal369@gmail.com>
2019-09-22 00:04:46 +0200

    Version 1 release

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# A twitter simple dashboard
+# A Simple Twitter dashboard
 
-under development
+A dashboard for any custom query with simple analysis tools
+
+![dashboard](res/dash.png)
+
+the dashboard runs in the local host, it's still a little bit slow, but that's an early version, most probably i will be working on it's performance.
+
+## Installation
+
+to use the dashboard follow these steps
+
+* install all the requirements
+
+```bash
+$pip install -r requirements.txt
+```
+
+Now you are ready to go !
+
+* start the app from `app.py`
+
+```bash
+$python app.py
+```
+
+you will find a message ending with the url like so
+
+```bash
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+```
+
+Jump into the link and you will directed to the landing page
+
+## Usage tips
+
+* The max number of tweets are set to 1000 and for only one month back, you can override these option in the `twitterdash/scraper.py` file.
+* Changing the `poolsize` in `twitterdash/scraper.py` will affect the speed of scraping but also the performance of your machine, don't get too far with it.
+
+![landing](res/landing.png)
+
+you can type your query and press analyse to get the dashboard, it's a little bit slow, but it works :D
+
+## References
+
+* Pages design were made possible by [Bootstrap studio](https://bootstrapstudio.io)
+* For Twitter scraping i used [twitterscraper](https://github.com/taspinar/twitterscraper) package, which is pretty useful !
+* Word cloud is pretty easy using [word_cloud](https://github.com/amueller/word_cloud) package.
+* Sentiment analysis here were made using [TextBlob](https://github.com/sloria/textblob) library.
--- twitterdash/routes.py
+++ twitterdash/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from textblob import TextBlob
-from twitterdash.scraper import get_last_half_year_tweets
+from twitterdash.scraper import get_last_half_year_tweets, get_last_month_tweets
 from twitterdash.preprocessing import process_text
 
 app = Flask(__name__)
@@ -16,10 +16,10 @@ def index():
 def dash():
     query = request.form.get("query")
     print("got query of", query)
-    tweets = get_last_half_year_tweets(query)
+    tweets = get_last_month_tweets(query)
     columns = ['fullname', 'is_retweet', 'likes',
                'replies', 'retweet_id', 'retweeter_userid', 'retweeter_username', 'retweets',
-               'text', 'timestamp', 'tweet_id', 'user_id', 'username']
+               'text', 'timestamp', 'tweet_id', 'user_id', 'username', 'tweet_url']
     tweets_df = pd.DataFrame(columns=columns)
 
     for tweet in tweets:
@@ -41,6 +41,12 @@ def dash():
     pos_sent = sentiment[True]
     neg_sent = sentiment[False]
 
+    # sort tweets by likes then retweets and get the top 6 tweets
+    tweets_df = tweets_df.sort_values(['retweets', 'likes'], ascending=False)
+    tweets_df = tweets_df.drop_duplicates('text')
+    tweets_df['text'] = tweets_df['text'].apply(lambda x: x.strip())
+    top_tweets = tweets_df.iloc[:3, :].to_dict("records")
+
     return render_template("dashboard.html", query=query, total_tweets=total_tweets,
     total_retweets=total_retweets, total_likes=total_likes, hashtags=zip(responses['hashtags'], styles),
-     cloud_sign=responses['cloud_sign'], negative_counts=neg_sent, positive_counts=pos_sent)
+     cloud_sign=responses['cloud_sign'], negative_counts=neg_sent, positive_counts=pos_sent, top_tweets=top_tweets)

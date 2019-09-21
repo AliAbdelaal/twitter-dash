795580b5e03b665ba58779786dbad2b2fd2fe5a6
Ali Abdel Aal <aliabdelaal369@gmail.com>
2019-09-21 02:39:53 +0200

    changed poolsize

--- twitterdash/scraper.py
+++ twitterdash/scraper.py
@@ -4,12 +4,12 @@ from twitterscraper import query_tweets
 
 def get_last_month_tweets(query, limit=1000, lang="en"):
     start_date = date.today() - timedelta(30)
-    tweets = query_tweets(query, limit, lang=lang, begindate=start_date)
+    tweets = query_tweets(query, limit,poolsize=10, lang=lang, begindate=start_date)
     print(f"got {len(tweets)} tweet.")
     return tweets
 
 def get_last_half_year_tweets(query, limit=1000, lang="en"):
     start_date = date.today() - timedelta(30*6)
-    tweets = query_tweets(query, limit, lang=lang, begindate=start_date)
+    tweets = query_tweets(query, limit,poolsize=10, lang=lang, begindate=start_date)
     print(f"got {len(tweets)} tweet.")
     return tweets
